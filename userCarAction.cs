61b79edbb934aea44d82e2b700b711ce1f74c7d2
etguy2 <guy.itach@campus.technion.ac.il>
2019-05-04 12:54:07 +0300

    userCarAction added expiration time for permits

--- userCarAction.cs
+++ userCarAction.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -16,6 +17,9 @@ namespace carSharing.userCarAction
         [FunctionName("userCarAction")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
+            // Const Vars
+            const int permit_validity_time = 100; // No. of minutes until permit expiration time.
+
             // parse query parameter
             string action = req.GetQueryNameValuePairs()
                 .FirstOrDefault(q => string.Compare(q.Key, "action", true) == 0)
@@ -41,16 +45,23 @@ namespace carSharing.userCarAction
             bool status = false;
             string _conn_str = System.Environment.GetEnvironmentVariable("sqldb_connection");
 
+            // Defines tiem time treshold for the permit
+            DateTime permit_expiration_treshold = DateTime.Now;
+            permit_expiration_treshold.AddHours(-3); // Subs 3 hours because Azure are stupid.
+            permit_expiration_treshold.AddMinutes(permit_validity_time);
+
             // Look for the right Permit in the DB
             string permit_query =  "SELECT COUNT(*) FROM Permits "
                                     + "INNER JOIN Users ON Users.id = Permits.user_id AND Permits.user_id = @user_id "
-                                    + "INNER JOIN Vehicles ON Vehicles.id = Permits.vehicle_id AND Permits.vehicle_id = @vehicle_id ";
+                                    + "INNER JOIN Vehicles ON Vehicles.id = Permits.vehicle_id AND Permits.vehicle_id = @vehicle_id "
+                                    + "AND Permits.time <= Convert(datetime, @permit_expiration_treshold'2019-05-4 08:53:53.840' )";
 
             using (SqlConnection conn = new SqlConnection(_conn_str)) {
                 conn.Open();
                 SqlCommand command = new SqlCommand(permit_query, conn);
                 command.Parameters.AddWithValue("@user_id", user_id);
                 command.Parameters.AddWithValue("@vehicle_id", vehicle_id);
+                command.Parameters.AddWithValue("@permit_expiration_treshold", permit_expiration_treshold.ToString("yyyy’-‘MM’-‘dd’T’HH’:’mm’:’ss"));
                 int rows = (int) command.ExecuteScalar();
                 if (rows >= 1) 
                     status = true;
