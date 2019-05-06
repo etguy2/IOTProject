91ab1f0fa51abbe494e0728998efe7c5f1b7c9ba
etguy2 <guy.itach@campus.technion.ac.il>
2019-05-06 09:14:38 +0300

    debug comm fixes

--- userCarAction.cs
+++ userCarAction.cs
@@ -71,7 +71,7 @@ namespace carSharing.userCarAction
                 update_checkin();
 
             } else {
-                response = new response(-2, "No permit " + permit_expiration_treshold.ToString() +  "-----" + _permit_validity_time);
+                response = new response(-2, "No permit");
             }
             return req.CreateResponse(HttpStatusCode.OK, response, JsonMediaTypeFormatter.DefaultMediaType);
         }
