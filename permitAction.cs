04732ec15272c95745652a155ffce9e0db33e18a
etguy2 <guy.itach@campus.technion.ac.il>
2019-05-25 10:07:29 +0300

    action by vehicle_id instead of permit_id

--- permitAction.cs
+++ permitAction.cs
@@ -21,7 +21,7 @@ namespace carSharing.PermitAction
 
             try {
                 // parse query parameter
-                int permit_id = Convert.ToInt32 ( utilitles.getURLVar(req, "permit_id") );
+                int vehicle_id = Convert.ToInt32 ( utilitles.getURLVar(req, "vehicle_id") );
                 int user_id = Convert.ToInt32 ( utilitles.getURLVar(req, "user_id") );
                 string login_hash = utilitles.getURLVar(req, "login_hash");
                 int action =  Convert.ToInt32 ( utilitles.getURLVar(req, "action") );
@@ -34,7 +34,7 @@ namespace carSharing.PermitAction
 
                 string[] new_status = new string[] { "DENIED", "APPROVED" };
 
-                update_permit_status(permit_id, user_id, new_status[action]);
+                update_permit_status(vehicle_id, user_id, new_status[action]);
                 response = new response(1, "Permit " + new_status[action]);
 
             } catch (CarSharingException ex) {
@@ -43,14 +43,14 @@ namespace carSharing.PermitAction
 
             return req.CreateResponse(HttpStatusCode.OK, response, JsonMediaTypeFormatter.DefaultMediaType);
         }
-        private static void update_permit_status(int permit_id, int user_id, string new_status) {
+        private static void update_permit_status(int vehicle_id, int user_id, string new_status) {
             DateTime checkin = DateTime.Now;
-            string update_checkin = "UPDATE Permits SET status = @status WHERE id = @permit_id AND user_id = @user_id";
+            string update_checkin = "UPDATE Permits SET status = @status WHERE vehicle_id = @vehicle_id AND user_id = @user_id";
             using (SqlConnection conn = new SqlConnection(_conn_str)) {
                 conn.Open();
                 SqlCommand command = new SqlCommand(update_checkin, conn);
                 command.Parameters.AddWithValue("@status", new_status);
-                command.Parameters.AddWithValue("@permit_id", permit_id);
+                command.Parameters.AddWithValue("@vehicle_id", vehicle_id);
                 command.Parameters.AddWithValue("@user_id", user_id);
                 command.ExecuteNonQuery();
                 conn.Close();
