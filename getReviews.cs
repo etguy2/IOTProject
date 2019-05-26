1d92137152c6c28f42e77687f1fd89261c1a3b00
etguy2 <guy.itach@campus.technion.ac.il>
2019-05-26 14:50:12 +0300

    yo

--- getReviews.cs
+++ getReviews.cs
@@ -31,8 +31,9 @@ namespace CarSharing.getReviews
 
                 response.status = 1;
                 r_response.status = 1;
-                r_response.avg_rate = getAvgReview(reviewee_id);
-                r_response.reviews = getReviewList(reviewee_id);
+                
+                //r_response.avg_rate = getAvgReview(reviewee_id);
+                //r_response.reviews = getReviewList(reviewee_id);
 
             } catch (CarSharingException ex) {
                 response = new response(ex.status_code, "Error: " + ex.info);
