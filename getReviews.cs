feb6cedfd0b453635478eca72f41b0b936316583
etguy2 <guy.itach@campus.technion.ac.il>
2019-05-26 15:06:53 +0300

    getReviews Order

--- getReviews.cs
+++ getReviews.cs
@@ -45,7 +45,7 @@ namespace CarSharing.getReviews
         private static double getAvgReview(int reviewee_id) {
             using (SqlConnection conn = new SqlConnection(_conn_str)) {
                 conn.Open();
-                string avg_review = "SELECT AVG(Cast(rate as Float)) FROM Reviews WHERE reviewee_id = @reviewee_id";
+                string avg_review = "SELECT AVG(Cast(rate as Float)) FROM Reviews WHERE reviewee_id = @reviewee_id ORDER BY id DESC";
                 SqlCommand command = new SqlCommand(avg_review, conn);
                 command.Parameters.AddWithValue("@reviewee_id", reviewee_id);
                 double avg = (double)command.ExecuteScalar();
