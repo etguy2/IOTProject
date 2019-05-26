323325635627b0b103f85fdca852f9962ac3edca
etguy2 <guy.itach@campus.technion.ac.il>
2019-05-26 15:03:02 +0300

    getReviews fix casting AVG rate

--- getReviews.cs
+++ getReviews.cs
@@ -45,10 +45,10 @@ namespace CarSharing.getReviews
         private static double getAvgReview(int reviewee_id) {
             using (SqlConnection conn = new SqlConnection(_conn_str)) {
                 conn.Open();
-                string avg_review = "SELECT AVG(rate) FROM Reviews WHERE reviewee_id = @reviewee_id";
+                string avg_review = "SELECT AVG(Cast(rate as Float)) FROM Reviews WHERE reviewee_id = @reviewee_id";
                 SqlCommand command = new SqlCommand(avg_review, conn);
                 command.Parameters.AddWithValue("@reviewee_id", reviewee_id);
-                double avg = (double)(int) command.ExecuteScalar();
+                double avg = (double)command.ExecuteScalar();
                 conn.Close();
                 return avg;
             }
